1fdba37336813438a2b85a7e77975a1f456fee84
kavaskar <kavaskarsundaramoorthi@gmail.com>
2024-05-24 22:49:26 +0530

    practiced some more

---
+++ python/size_prac.py
@@ -0,0 +1,29 @@
+
+import sys
+
+'''This function provides the capacity, size and space left in the list.
+You can invoke it to get the details of the list'''
+
+def list_details(lst):
+    #Number of elements that can be stored in the list
+    print("size:",sys.getsizeof(lst))
+    print("Capacity:", (sys.getsizeof(lst)-64)//8)
+
+    #Number of elements in the list
+    print("Size:", len(lst))
+
+    print(len(lst*4))
+    #Number of elements that can be accommodated in the space left
+    print("Space Left:", ((sys.getsizeof(lst)-64) - len(lst*4))//8)
+
+    #formula changes based on the system architecture
+    #(size-64)/4 for 32 bit machines and
+    #(size-64)/8 for 64 bit machines
+
+    # 64, 64 - size of an empty list based on machine
+    # 4, 8 - size of a single element in the list based on machine
+
+marias_lst=[1]
+print("Empty list created!!!")
+print("List details:")
+list_details(marias_lst)
