50f0b187435740c99255b7fb20e115328dc16c25
kavaskar786 <kavaskar.s@mca.christuniversity.in>
2024-08-17 09:19:59 +0530

    added few more changes

--- htm_proj/login_page/script.js
+++ htm_proj/login_page/script.js
@@ -1,7 +1,10 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,14 +19,26 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-//inputs
-const email = document.getElementById("email").value();
-const password = document.getElementById("password").value();
-
 //submit button
 const submit = document.getElementById("submit");
 submit.addEventListener("click",function(event) {
-    event.preventDefault();
-    alert(5);
+    event.preventDefault()
+    const auth = getAuth();
+    //inputs
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        alert("creating account");
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage)
+        // ..
+    });
 });
 
