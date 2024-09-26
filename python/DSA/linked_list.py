6373f23e90474b11d0cc5627249f83ce2ba4bcb4
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-09-26 23:30:12 +0530

    added few more changes

---
+++ python/DSA/linked_list.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,data,next):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list():
+        print()
