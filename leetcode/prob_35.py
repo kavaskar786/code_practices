00f1a409bbf5b57fce5501d5e5d2a481cb61edbe
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-10-02 22:13:34 +0530

    added few more changes

--- leetcode/prob_35.py
+++ leetcode/prob_35.py
@@ -5,12 +5,12 @@ class Solution:
 
         while left <= right:
             mid = (left + right) // 2
-
+            
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
                 right = mid - 1
             else:
                 left = mid + 1
-        
+       
         return left
