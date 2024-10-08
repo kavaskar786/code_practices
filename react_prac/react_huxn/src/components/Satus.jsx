83450a8449c7aac85466fd9748f893c7816aba6a
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-10-08 10:55:02 +0530

    added fe more things

--- react_prac/react_huxn/src/components/Satus.jsx
+++ react_prac/react_huxn/src/components/Satus.jsx
@@ -5,12 +5,18 @@ const Satus = ({ isAdmin, isLoggedIn }) => {
   const cngVal = () => {
     setX(x + 100);
   };
+  let arr = ["hello", "hi", "i am testing"];
   return (
     <div>
       {isAdmin && isLoggedIn && <h1> Welcome Admin</h1>}
       {!isAdmin && isLoggedIn && <h1> Welcome User</h1>}
       {console.log(isAdmin, isLoggedIn)}
       <p>{x}</p>
+      <ul>
+        {arr.map((item, inx) => (
+          <li key={inx}>{item}</li>
+        ))}
+      </ul>
       <button onClick={cngVal}>click me!</button>
     </div>
   );
