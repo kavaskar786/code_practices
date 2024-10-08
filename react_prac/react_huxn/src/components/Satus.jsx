b01e543a5a56ad5da5d458f1a5d1c54628192226
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-10-08 10:38:34 +0530

    added few more things there

--- react_prac/react_huxn/src/components/Satus.jsx
+++ react_prac/react_huxn/src/components/Satus.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Satus = ({ isAdmin, isLoggedIn }) => {
+  let [x, setX] = useState(200);
+  const cngVal = () => {
+    setX(x + 100);
+  };
   return (
     <div>
       {isAdmin && isLoggedIn && <h1> Welcome Admin</h1>}
       {!isAdmin && isLoggedIn && <h1> Welcome User</h1>}
       {console.log(isAdmin, isLoggedIn)}
+      <p>{x}</p>
+      <button onClick={cngVal}>click me!</button>
     </div>
   );
 };
