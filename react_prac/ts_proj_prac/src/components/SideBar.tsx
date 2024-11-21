4c308ea577cd1c79d3cc3b663cc3e938f5f60686
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-11-21 12:33:48 +0530

    added few more changes

--- react_prac/ts_proj_prac/src/components/SideBar.tsx
+++ react_prac/ts_proj_prac/src/components/SideBar.tsx
@@ -1,4 +1,8 @@
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="sidebar fixed bg-black w-20 top-0 left-0 text-white ">
+      <ul className=""></ul>
+    </div>
+  );
 };
 export default SideBar;
