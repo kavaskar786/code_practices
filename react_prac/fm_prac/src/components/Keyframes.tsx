b5253867a91dfa120e30cf107616d27a97856950
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-12-20 22:37:37 +0530

    added few more changes

--- react_prac/fm_prac/src/components/Keyframes.tsx
+++ react_prac/fm_prac/src/components/Keyframes.tsx
@@ -1,12 +1,12 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 const Keyframes = () => {
   return (
     <div className="flex items-center justify-center flex-row flex-wrap gap-4">
       <motion.div
         className="h-20 w-20 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg flex items-center justify-center text-white  text-[10px]"
         initial={{ scale: 0 }}
-        animate={{ scale: [0, 1, 0.5, 1] }}
-        transition={{ duration: 4 }}
+        animate={{ scale: [1, 1.1, 1] }}
+        transition={{ duration: 0.5, repeat: Infinity, ease: "easeInOut" }}
       >
         scale: [0, 1, 0.5, 1]
       </motion.div>
