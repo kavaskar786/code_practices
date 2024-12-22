97a38999e49b1d34b9504fcf35f5cd37ee6a16e3
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-12-23 01:30:54 +0530

    added few more changes

--- react_prac/route_prac/src/App.tsx
+++ react_prac/route_prac/src/App.tsx
@@ -1,4 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+
 const App = () => {
-  return <div className="text-3xl">App</div>;
+  return (
+    <div className="min-h-screen bg-[#fefcfcfa]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contacts" element={<Contacts />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
---
+++ react_prac/route_prac/src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import { spring } from "motion";
+import { motion } from "motion/react";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between w-[85%] mx-auto h-[8vh]  ">
+      <div className="logo font-semibold">Kavaskar's Website</div>
+      <nav className="flex items-center justify-center gap-4">
+        <motion.div
+          animate="rest"
+          whileHover="hover"
+          className="cursor-pointer	"
+        >
+          Home
+          <motion.div
+            variants={{ rest: { width: "0%" }, hover: { width: "100%" } }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="h-[2.5px] bg-black"
+          ></motion.div>
+        </motion.div>
+        <motion.div
+          animate="rest"
+          whileHover="hover"
+          className="cursor-pointer	"
+        >
+          Product
+          <motion.div
+            variants={{ rest: { width: "0%" }, hover: { width: "100%" } }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="h-[2.5px] bg-black"
+          ></motion.div>
+        </motion.div>
+        <motion.div
+          animate="rest"
+          whileHover="hover"
+          className="cursor-pointer	"
+        >
+          About
+          <motion.div
+            variants={{ rest: { width: "0%" }, hover: { width: "100%" } }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="h-[2.5px] bg-black"
+          ></motion.div>
+        </motion.div>
+        <motion.div
+          animate="rest"
+          whileHover="hover"
+          className="cursor-pointer	"
+        >
+          Contact
+          <motion.div
+            variants={{ rest: { width: "0%" }, hover: { width: "100%" } }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="h-[2.5px] bg-black"
+          ></motion.div>
+        </motion.div>
+      </nav>
+      <div className="flex items-center justify-center gap-4">
+        <button className="bg-black text-white px-4 py-2 rounded-3xl hover:bg-[#636363]">
+          Sign up
+        </button>
+        <button className="bg-black text-white px-4 py-2 rounded-3xl hover:bg-[#676666]">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
