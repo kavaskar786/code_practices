68d425e1647707af165ad15f5f3758f6c92d0b68
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2024-12-24 23:10:31 +0530

    added pagination into this page

--- react_prac/route_prac/src/components/UsersList.tsx
+++ react_prac/route_prac/src/components/UsersList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 interface test {
@@ -8,18 +9,48 @@ interface test {
 
 const UsersList = () => {
   const data = useLoaderData();
+  const [itemsLimit, setItemLimit] = useState<number>(0);
   return (
     <div>
-      {data.map(({ id, name, email }: test) => (
-        <Link to={id.toString()} key={id}>
-          {" "}
-          <div className="mt-10">
-            {/* <p>{id}</p> */}
-            <p>{name}</p>
-            <p>{email}</p>
+      {data
+        .slice(itemsLimit, itemsLimit + 2)
+        .map(({ id, name, email }: test) => (
+          <Link to={id.toString()} key={id}>
+            {" "}
+            <div className="mt-10">
+              {/* <p>{id}</p> */}
+              <p>{name}</p>
+              <p>{email}</p>
+            </div>
+          </Link>
+        ))}
+      <div className="flex items-center justify-center flex-row gap-4">
+        {[...Array(data.length / 2 + 2)].map((_, index) => (
+          <div key={index}>
+            {index === 0 || index === data.length / 2 + 1 ? (
+              index === 0 ? (
+                <button
+                  onClick={() => setItemLimit(itemsLimit - 2)}
+                  disabled={itemsLimit === 0}
+                >
+                  previous
+                </button>
+              ) : (
+                <button
+                  onClick={() => setItemLimit(itemsLimit + 2)}
+                  disabled={itemsLimit === data.length - 2}
+                >
+                  Next
+                </button>
+              )
+            ) : (
+              <button onClick={() => setItemLimit(index * 2 - 2)}>
+                {index}
+              </button>
+            )}
           </div>
-        </Link>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
