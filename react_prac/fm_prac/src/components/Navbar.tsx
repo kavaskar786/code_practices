03d7c09dda05c3927c9c5bd82ed96363b0e8ba8b
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2025-01-02 10:22:57 +0530

    added some more changes also

--- react_prac/fm_prac/src/components/Navbar.tsx
+++ react_prac/fm_prac/src/components/Navbar.tsx
@@ -3,20 +3,20 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const navItems = ["/", "Gestrues", "Loading", "Animation"];
   return (
-    <div
-      className={`relative flex items-center justify-between w-[90%] mx-auto h-[8vh]`}
-    >
-      <div className="logo text-3xl">Kavaskar's</div>
-      <nav className="flex items-center justify-center gap-4">
-        {navItems.map((item, index) => (
-          <NavLink to={item} key={index}>
-            <div>{item === "/" ? "FlipCard" : item}</div>
-          </NavLink>
-        ))}
-      </nav>
-      <div className="flex items-center justify-center gap-4">
-        <button>Sign up</button>
-        <button>Login</button>
+    <div className="bg-gradient-to-r from-slate-900 to-slate-400 h-[8vh] w-[100%] flex items-center justify-center text-white">
+      <div className={` flex items-center justify-between w-[90%] mx-auto  `}>
+        <div className="logo text-3xl">Kavaskar's</div>
+        <nav className="flex items-center justify-center gap-4">
+          {navItems.map((item, index) => (
+            <NavLink to={item} key={index}>
+              <div>{item === "/" ? "FlipCard" : item}</div>
+            </NavLink>
+          ))}
+        </nav>
+        <div className="flex items-center justify-center gap-4">
+          <button>Sign up</button>
+          <button>Login</button>
+        </div>
       </div>
     </div>
   );
