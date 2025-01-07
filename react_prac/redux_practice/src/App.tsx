08de5cfb81b46300510088bbb41948deaeb93453
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2025-01-07 15:32:16 +0530

    feat: refactor App component to use Redux and add Home component

--- react_prac/redux_practice/src/App.tsx
+++ react_prac/redux_practice/src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import Home from "./Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+);
