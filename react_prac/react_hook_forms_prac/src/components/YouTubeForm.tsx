d4e3cd79c3541fdd98000564f05a3195468b2794
Kavaskar <fflegendguys@gmail.com>
2026-03-08 10:10:12 +0530

    feat: add devtool for form debugging and restructure youtubeform component

--- react_prac/react_hook_forms_prac/src/components/YouTubeForm.tsx
+++ react_prac/react_hook_forms_prac/src/components/YouTubeForm.tsx
@@ -1,20 +1,21 @@
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export const YouTubeForm = () => {
-  const { register } = useForm();
+  const { register, control } = useForm();
 
   return (
-    <form>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" {...register("username")} />
-
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" {...register("email")} />
-
-      <label htmlFor="channel">Channel:</label>
-      <input type="text" id="channel" {...register("channel")} />
-
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" {...register("username")} />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" {...register("email")} />
+        <label htmlFor="channel">Channel:</label>
+        <input type="text" id="channel" {...register("channel")} />
+        <button type="submit">Submit</button>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
