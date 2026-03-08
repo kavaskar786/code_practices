bfa475dde1f973f95bc14dc22ab1c183bb285d87
Kavaskar <fflegendguys@gmail.com>
2026-03-08 10:01:07 +0530

    feat: simplify input registration in youtubeform component using spread operator

--- react_prac/react_hook_forms_prac/src/components/YouTubeForm.tsx
+++ react_prac/react_hook_forms_prac/src/components/YouTubeForm.tsx
@@ -1,24 +1,18 @@
+import { useForm } from "react-hook-form";
+
 export const YouTubeForm = () => {
   const { register } = useForm();
 
-  const { name, ref, onChange, onBlur } = register("username");
   return (
     <form>
       <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        name={name}
-        ref={ref}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
+      <input type="text" id="username" {...register("username")} />
 
       <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" />
+      <input type="email" id="email" {...register("email")} />
 
       <label htmlFor="channel">Channel:</label>
-      <input type="text" id="channel" name="channel" />
+      <input type="text" id="channel" {...register("channel")} />
 
       <button type="submit">Submit</button>
     </form>
